4c5dee0a5610c0818661b0eb4a9f87418701cf9c
anusha9baditha <anusha@codegnan.com>
2026-08-27 12:57:17 +0530

    flash,session

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
-from flask import Flask,request,redirect,url_for,render_template
+from flask import Flask,request,redirect,url_for,render_template,session,flash
+from flask_session import Session #stores secure server side session 
 from otp import genotp
 from cmail import send_mail
 from mysql.connector import (connection)
 mydb=connection.MySQLConnection(user='root',host='localhost',password='admin@123',db='snm034db')
 from datetime import datetime,timedelta
 app=Flask(__name__)
+app.secret_key='Codegnan123'
+app.config['SESSION_TYPE']='filesystem'
+Session(app) #it intialize the session data
 @app.route('/',methods=['GET'])
 def index():
     return render_template('index.html')
@@ -25,7 +29,8 @@ def register():
             otp_expiry_time=datetime.now()+timedelta(minutes=5)
             if  db_response:
                 if db_response[0]=='active':
-                    return 'User already existed'
+                    flash('User already existed')
+                    return redirect(url_for('register'))
                 cursor.execute('update userdata set otp=%s,otp_expiry_time=%s,account_status=%s where useremail=%s',[server_otp,otp_expiry_time,'inactive',useremail])
             else:
                 cursor.execute('insert into userdata(username,useremail,userpassword,otp,otp_expiry_time,account_status) values(%s,%s,%s,%s,%s,%s)',[username,useremail,userpassword,server_otp,otp_expiry_time,'inactive'])
@@ -34,6 +39,7 @@ def register():
             subject='User OTP Verification for SNM Application'
             body=f'Hello {username} use thye given otp {server_otp}'
             send_mail(subject=subject,body=body,to=useremail)
+            flash('OTP has been sent to given email')
             return redirect(url_for('otp_verify',useremail=useremail))
         return render_template('register.html')
     except Exception as e:
@@ -54,16 +60,21 @@ def otp_verify(useremail):
             stored_userdata=cursor.fetchone()
             print(stored_userdata)
             if not stored_userdata:
-                return 'user not found in db'
+                flash('user not found in db')
+                return redirect(url_for('otp_verify',useremail=useremail))
             if user_otp_time > stored_userdata[1]:
-                return 'OTP Expiried pls try again'
+                flash('OTP Expiried pls try again')
+                return redirect(url_for('otp_verify',useremail=useremail))
             if stored_userdata[2]=='active':
-                return 'User already Existed pls login'
+                flash('User already Existed pls login')
+                return redirect(url_for('otp_verify',useremail=useremail))
             if user_otp !=stored_userdata[0]:
-                return 'INvalid otp pls check again'
+                flash('INvalid otp pls check again')
+                return redirect(url_for('otp_verify',useremail=useremail))
             cursor.execute("update  userdata set otp=null,otp_expiry_time=null,account_status='active' where useremail=%s",[useremail])
             mydb.commit()
             cursor.close()
+            flash('OTp verified successfuly')
             return redirect(url_for('login'))
         return render_template('otp.html')
     except Exception as e:
@@ -82,17 +93,29 @@ def login():
         user_data=cursor.fetchone()
         print(user_data)
         if not user_data:
-            return 'Email Not found pls try again'
+            flash('Email Not found pls try again')
+            return redirect(url_for('login'))
         if user_data[0]=='active':
             if user_data[1]==login_password:
+                #create session data to remember the user,user prefernces 
+                #like a dictionary add a key 
+                print(session,'before')
+                session['user']=login_useremail
+                print(session,'after')
+                flash('Login successfull')
                 return redirect(url_for('dashboard'))
             else:
-                return 'Invalid password'
+                flash('Invalid password')
+                return redirect(url_for('login'))
         else:
-            return 'User Not verified pls register again'
+            flash('User Not verified pls register again')
+            return redirect(url_for('login'))
     return render_template('login.html')
 @app.route('/dashboard',methods=['GET'])
 def dashboard():
+    if not session.get('user'):
+        flash('To access dashboard pls login first')
+        return redirect(url_for('login'))
     return render_template('dashboard.html')
 @app.route('/addnotes',methods=['GET','POST'])
 def addnotes():
@@ -100,5 +123,15 @@ def addnotes():
 @app.route('/viewallnotes',methods=['GET','POST'])
 def viewallnotes():
     return render_template('viewallnotes.html')
+@app.route('/userlogout',methods=['GET'])
+def userlogout():
+    if not session.get('user'):
+        flash('To logout pls login first')
+        return redirect(url_for('login'))
+    session.pop('user') #deletes nuser session data
+    return redirect(url_for('index'))
+
+
+
 if __name__=='__main__':
     app.run(debug=True,use_reloader=True)
